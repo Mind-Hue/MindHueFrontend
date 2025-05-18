8219020e07d99adfc3d8c26871d5dad84d407494
Paola Morales <dissenyaspasia@gmail.com>
2025-05-18 23:05:21 +0200

    add assets for Home page creation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/Header/Header";
-import Footer from "../../components/Footer/Footer"; // Asegúrate de que este archivo exista
+import Footer from "../../components/Footer/Footer"; 
+import "./Home.css";
 
 function Home() {
   return (
