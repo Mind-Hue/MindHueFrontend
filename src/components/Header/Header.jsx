2b6578cd9a5422921b039a862624bfb1ff2d7374
Paola Morales <dissenyaspasia@gmail.com>
2025-05-21 20:44:31 +0200

    make logo in header clickable to navigate to home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import "./Header.css"; 
+import { Link } from "react-router-dom";
+import "./Header.css";
 import Logo from "../../assets/images/WhiteLogo.png"; // Asegúrate de que la ruta sea correcta
 
 export default function Header() {
   return (
     <header className="header">
       <div className="header-content">
-        <img src={Logo} alt="Logo" className="logo" />
+        <Link to="/">
+          <img src={Logo} alt="Logo" className="logo" />
+        </Link>
         <div className="menu-icon">
           <div></div>
           <div></div>
