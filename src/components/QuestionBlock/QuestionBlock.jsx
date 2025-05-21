48f5e95e64167b152d90b3e17d47129531e81843
Paola Morales <dissenyaspasia@gmail.com>
2025-05-21 13:31:50 +0200

    display questions from backend with input fields in QuestionBlock component

--- src/components/QuestionBlock/QuestionBlock.jsx
+++ src/components/QuestionBlock/QuestionBlock.jsx
@@ -4,9 +4,9 @@ import "./QuestionBlock.css";
 const QuestionBlock = ({ questions }) => {
   return (
     <div className="question-box">
-      {questions.map((q, i) => (
-        <div className="question-field" key={i}>
-          <label>{q}</label>
+      {questions.map((q) => (
+        <div className="question-field" key={q.id}>
+          <label>{q.question}</label>
           <input type="text" placeholder="Escribe tu respuesta..." />
         </div>
       ))}
